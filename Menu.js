c54ff0f9c04f20e85ef25365992739db27c7cad8
lucas cerqueira do nascimento <terrarialucas123@gmail.com>
2025-03-08 17:13:07 -0300

    Animação de pixel

--- Menu.js
+++ Menu.js
@@ -6,33 +6,38 @@ const Men_container = document.getElementById("Menu_container")
      const List_ = document.querySelectorAll('li.list_')
       const div_search = document.querySelector(".block_search")
        const inp_search = document.querySelector('.isearch')
-
-
+ 
+ // variavel para o carrossel
+ let contador = 0
  let obj_funcoes = {
         // função para evento de clicar fora do conteudo 
-    f_event_cli_out(event,Arg1,Arg2,Arg3,Valor){
+    f_event_cli_out(event,Arg1,Arg2,Arg3,Valor,V_class){
         if(!Arg1.contains(event.target) && !Arg2.contains(event.target)){
            Arg3.style.display = Valor
-           console.log("teste")
+           Arg3.classList.remove(V_class)
         }
+    },
+    f_event_carrossel(novoContador,itens_class,V_class){
+        itens_class[contador].classList.remove(V_class)
+         contador = (novoContador + itens_class.length) % itens_class.length
+        itens_class[contador].classList.add(V_class)
     }
  }
 
 document.addEventListener('click',(evento) =>{
     List_.forEach(() =>{
-        let key_search = List_[1]
-        obj_funcoes.f_event_cli_out(evento,key_search,inp_search,div_search,"none")
+        let key_search = List_[1] // search
+  obj_funcoes.f_event_cli_out(evento,key_search,inp_search,div_search,"none",null)
     })
 })
 
-List_.forEach((Lista, keys_list) => {
+List_.forEach((Lista, keys_list) => { 
     Lista.addEventListener("click", () => {
         if(keys_list == 1){
             div_search.style.display = "flex"
             div_search.classList.add('On_Animation')
             inp_search.style.display = "block"
             inp_search.focus()
-           
         }else{
             div_search.style.display = "none"
             inp_search.style.display = "none"
@@ -83,8 +88,7 @@ const obj_ani_Menu = {
             obj_ani_Menu.Esconder_menu(click_Troca_estado)
             obj_ani_Menu.Esconder_Itens_Memu(click_Troca_estado)
         }
-       /* transform: rotateZ(180deg);
-       */
+       /* transform: rotateZ(180deg);*/
        burger.style.transition = "transform 0.2s ease" ;
 
        click_Troca_estado = !click_Troca_estado // true = false || false = true
@@ -104,6 +108,23 @@ const obj_ani_Menu = {
         remover_fixar.style.removeProperty("left")
     }
 }
+
+    if(window.innerWidth > 767){
+        document.body.style.transition = "width 0.2s linear"
+    }else{
+  // Sistema pra animação de pixels
+    for(let i = 0; i < 10; i++ ){
+        let pixels  = document.createElement("div")
+         pixels.classList.add("Anima")
+         // posições,delay,direções
+         pixels.style.top = Math.random() * 100 + "vh"
+          pixels.style.animationDelay = Math.random() * 3 + "s"
+           pixels.style.animationDuration = Math.random() * 2 + 3 + "s"
+        
+        document.body.appendChild(pixels)
+      }
+    }
+
     if(window.innerHeight > 100){
         event_scroll_Janela.Janela_down(Men_container)
     }else{
